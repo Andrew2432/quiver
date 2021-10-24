49b9355138230d7fe43fdfa4dbfdeea38a0039d1
Andrew2432 <drew.devops@gmail.com>
2021-10-24 17:55:24 +0530

    build basic Strapi project

---
+++ epsilon/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7d0acb6d7fa0ddba98204357196ceddf'),
+    },
+  },
+});
---
+++ epsilon/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '468e3eb7-9933-4b4a-bd18-128850f188c5'
+};
