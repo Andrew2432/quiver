24bbce0c0e5285e07c5265df113845e02312027d
Andrew2432 <drew.devops@gmail.com>
2021-10-29 19:29:37 +0530

    dockerize strapi cms

--- epsilon/config/database.js
+++ epsilon/config/database.js
@@ -1,18 +1,18 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '0.0.0.0'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'portfolio'),
-        username: env('DATABASE_USERNAME', 'andrew'),
-        password: env('DATABASE_PASSWORD', 'andrew'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: "postgres",
+        host: env("DATABASE_HOST"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        ssl: env.bool("DATABASE_SSL"),
       },
-      options: {}
+      options: {},
     },
   },
 });
--- epsilon/config/server.js
+++ epsilon/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST"),
+  port: env.int("PORT"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '7d0acb6d7fa0ddba98204357196ceddf'),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
 });
