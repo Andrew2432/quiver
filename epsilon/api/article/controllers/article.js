5002cc9a2438ef787f4e7dfcd10de30cae0108dc
Andrew2432 <drew.devops@gmail.com>
2021-11-05 14:39:05 +0530

    create views and hits api routes

--- epsilon/api/article/controllers/article.js
+++ epsilon/api/article/controllers/article.js
@@ -1,4 +1,4 @@
-const { sanitizeEntity } = require('strapi-utils');
+const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
   /**
@@ -12,5 +12,33 @@ module.exports = {
 
     const entity = await strapi.services.article.findOne({ slug });
     return sanitizeEntity(entity, { model: strapi.models.article });
+  },
+
+  async addViews(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.article.getSanitizedEntity(id);
+
+    if (entity == null) {
+      return strapi.services.article.articleNotFound(ctx);
+    }
+
+    await strapi.services.article.incrementArticleViews(entity);
+
+    return strapi.services.article.viewsUpdateSuccess(ctx);
+  },
+
+  async addHits(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.article.getSanitizedEntity(id);
+
+    if (entity == null) {
+      return strapi.services.article.articleNotFound(ctx);
+    }
+
+    await strapi.services.article.incrementArticleHits(entity);
+
+    return strapi.services.article.hitsUpdateSuccess(ctx);
   },
 };
--- epsilon/api/article/services/article.js
+++ epsilon/api/article/services/article.js
@@ -1,8 +1,65 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  async getSanitizedEntity(id) {
+    const entity = await strapi.services.article.findOne({ id });
+
+    if (entity == null) {
+      return null;
+    }
+
+    return sanitizeEntity(entity, {
+      model: strapi.models.article,
+    });
+  },
+
+  async incrementArticleViews(article) {
+    const newView = article.views == null ? 1 : parseInt(article.views) + 1;
+
+    const updatedEntity = await strapi.query("article").update(
+      { id: article.id },
+      {
+        views: newView,
+      }
+    );
+
+    return updatedEntity;
+  },
+
+  async incrementArticleHits(article) {
+    const newHits = article.hits == null ? 1 : parseInt(article.hits) + 1;
+
+    const updatedEntity = await strapi.query("article").update(
+      { id: article.id },
+      {
+        hits: newHits,
+      }
+    );
+
+    return updatedEntity;
+  },
+
+  articleNotFound(ctx) {
+    ctx.response.status = 404;
+    ctx.response.message = "Article not found";
+    return ctx.response;
+  },
+
+  viewsUpdateSuccess(ctx) {
+    ctx.response.status = 200;
+    ctx.response.message = "View updated successfully";
+    return ctx.response;
+  },
+
+  hitsUpdateSuccess(ctx) {
+    ctx.response.status = 200;
+    ctx.response.message = "Hits updated successfully";
+    return ctx.response;
+  },
+};
