c566c1d4c7ed0ef6051a946da29bcfcf465e41ab
Andrew2432 <drew.devops@gmail.com>
2021-11-06 08:47:54 +0530

    update code component

--- phi/src/components/code/Code.tsx
+++ phi/src/components/code/Code.tsx
@@ -1,8 +1,14 @@
-import { CodeProps } from './CodeProps';
+import { CSSProperties } from "react";
+import { CodeProps } from "./CodeProps";
+
+const codeStyles: CSSProperties = {
+  fontSize: "1.3rem",
+  fontFamily: "Hack",
+};
 
 function Code({ language, children }: CodeProps) {
   return (
-    <pre className={`language-${language}`}>
+    <pre className={`language-${language}`} style={codeStyles}>
       <code>{children}</code>
     </pre>
   );
